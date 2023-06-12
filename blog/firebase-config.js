65d81defdffefcf5cc4f7482215e41311b313b4b
LeticiaGiVi <leticia.villegas@outlook.com>
2023-06-12 12:26:43 -0300

    aula 12-6 webdesing

---
+++ blog/firebase-config.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyC80mC6Ery_hENioqAWBd5Vd_IlMKqCJks",
+    authDomain: "blog-b8130.firebaseapp.com",
+    projectId: "blog-b8130",
+    storageBucket: "blog-b8130.appspot.com",
+    messagingSenderId: "99947105046",
+    appId: "1:99947105046:web:1bc7d71ef938378e711e37"
+  };
+  
+  const app = initializeApp(firebaseConfig);
